1b83962e42b2162c5466b13a608a12437943b433
Daniel Costa <danielcosta@inf.ufg.br>
2016-04-10 17:16:17 -0300

    Signed-off-by: Daniel Costa <danielcosta@inf.ufg.br>

---
+++ build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['scratch/ns3.25-temp-debug', 'scratch/subdir/ns3.25-subdir-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
